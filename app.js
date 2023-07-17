6c4ca93716523d56574a5e3a52e41af9844300e9
Carlos Eduardo Carmo do Val <ceval@banestes.com.br>
2023-07-17 17:59:51 -0300

    Realizada inserção de imagens através do JS com detectção de tamanho de tela

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const url = "../data/data.json";
+const screenWidth = window.screen.availWidth;
+console.log(screenWidth);
 const product = document.getElementById("product");
 const item = document.getElementById("item");
 const description = document.getElementById("description");
 const price = document.getElementById("price");
 const oldPrice = document.getElementById("oldPrice");
+const left = document.getElementById("left");
 
 fetch(url)
     .then(response => response.json())
     .then(data => use(data));
-
 
 function use(data){
     console.log(data);
@@ -16,6 +18,18 @@ function use(data){
     let text = "";
     let prices = 123456.789;
     data.forEach(element => {
+        
+        const image = document.createElement('img');
+        image.setAttribute('alt', "Product image");
+
+        if (screenWidth < 600) {
+            image.setAttribute('src', element.mobileDesign);
+        } else {
+            image.setAttribute('src', element.desktopDesign);
+        };
+
+        left.appendChild(image);
+
         text = document.createTextNode((element.product).toUpperCase());
         product.appendChild(text);
         
@@ -30,5 +44,9 @@ function use(data){
 
         prices = document.createTextNode(new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(element.oldPrice));
         oldPrice.appendChild(prices);
+
+        
+
+
     });
 }
