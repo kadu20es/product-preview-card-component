69e2b8288378edc927180c17b41603d9775f7331
Carlos Eduardo Carmo do Val <ceval@banestes.com.br>
2023-07-17 17:02:38 -0300

    Finalizada construção do JS e remoção dos textos do HTML

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const url = "../data/data.json";
+const product = document.getElementById("product");
+const item = document.getElementById("item");
+const description = document.getElementById("description");
+const price = document.getElementById("price");
+const oldPrice = document.getElementById("oldPrice");
+
 fetch(url)
     .then(response => response.json())
-    .then(data => tratarData(data));
+    .then(data => use(data));
 
 
-function tratarData(data){
+function use(data){
     console.log(data);
+
+    let text = "";
+    let prices = 123456.789;
+    data.forEach(element => {
+        text = document.createTextNode((element.product).toUpperCase());
+        product.appendChild(text);
+        
+        text = document.createTextNode(element.item);
+        item.appendChild(text);
+
+        text = document.createTextNode(element.description);
+        description.appendChild(text);
+
+        prices = document.createTextNode(new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(element.price));
+        price.appendChild(prices);
+
+        prices = document.createTextNode(new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(element.oldPrice));
+        oldPrice.appendChild(prices);
+    });
 }
